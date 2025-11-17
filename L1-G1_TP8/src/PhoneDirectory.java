18db147b9979657ef3cb920c77176a7218c985d0
Dima Dalloul <dima.dalloul@univ-grenoble-alpes.fr>
2025-11-17 15:39:25 +0100

    Petite coquille dans TP8

--- L1-G1_TP8/src/PhoneDirectory.java
+++ L1-G1_TP8/src/PhoneDirectory.java
@@ -3,7 +3,7 @@ import java.io.FileWriter;
 import java.util.Scanner;
 
 public class PhoneDirectory {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // Question a : create the 2 arrays
         String[] arrayNames = new String[100];
         String[] arrayNumbers = new String[100];
@@ -33,14 +33,14 @@ public class PhoneDirectory {
         char userInput;
 
         // We enter the loop
-        do{
+        do {
             // We let the user know his/her options
             System.out.println("(a)dd (f)ind (l)ist (d)elete (s)ave (r)estore (q)uit :");
             // We get the first letter
             userInput = s.nextLine().charAt(0);
 
             // We're going to use a switch this time
-            switch (userInput){
+            switch (userInput) {
                 case 'l':
                     // Display the phone directory
                     displayPhoneDirectory(arrayNames, arrayNumbers);
@@ -103,10 +103,10 @@ public class PhoneDirectory {
     (une ligne par personne) et appelez-la dans le main.
     Cette action prend les deux tableaux en paramètre
      */
-    public static void displayPhoneDirectory(String[] arrayNames, String[] arrayNumbers){
+    public static void displayPhoneDirectory(String[] arrayNames, String[] arrayNumbers) {
         // We loop through both arrays
-        for(int i = 0; i < arrayNames.length; i++){
-            if(!arrayNames[i].equals("_END")) {
+        for (int i = 0; i < arrayNames.length; i++) {
+            if (!arrayNames[i].equals("_END")) {
                 // if I didn't reach the end, I should show the line
                 System.out.println(arrayNames[i] + " / " + arrayNumbers[i]);
             } else {
@@ -121,8 +121,8 @@ public class PhoneDirectory {
      * le nom n'existe pas. Complétez le main en appelant cette fonction
      */
     private static String searchPhoneDirectory(String[] arrayNames, String[] arrayNumbers, String name) {
-        for(int i = 0; (i < arrayNames.length) && (!arrayNames[i].equals("_END")); i++){
-            if(arrayNames[i].toLowerCase().equals(name)){
+        for (int i = 0; (i < arrayNames.length) && (!arrayNames[i].equals("_END")); i++) {
+            if (arrayNames[i].toLowerCase().equals(name)) {
                 return arrayNumbers[i];
             }
         }
@@ -132,14 +132,15 @@ public class PhoneDirectory {
 
     /**
      *
-     Bonus. Modifier la recherche pour tirer parti du fait que les éléments sont ordonnés.
-     Vous pourrez ainsi programmer une recherche dichotomique sur les éléments du tableau
-     entre les indices inf et sup (initialement, 0 et tab.length-1). Le principe est de
-     comparer l’élément à rechercher avec l’élément médian tab[m], c’est-à-dire l’élément au
-     milieu du tableau, d’indice m=(inf+sup)/2. Si tab[m] est l’élément recherché, on arrête.
-     Si tab[m] se situe après l’élément recherché dans l’ordre lexicographique, on recommence
-     la recherche sur le sous-tableau entre les indices 0 et m-1, sinon, on recommence sur le
-     sous-tableau entre les indices m+1 et tab.length-1.
+     * Bonus. Modifier la recherche pour tirer parti du fait que les éléments sont ordonnés.
+     * Vous pourrez ainsi programmer une recherche dichotomique sur les éléments du tableau
+     * entre les indices inf et sup (initialement, 0 et tab.length-1). Le principe est de
+     * comparer l’élément à rechercher avec l’élément médian tab[m], c’est-à-dire l’élément au
+     * milieu du tableau, d’indice m=(inf+sup)/2. Si tab[m] est l’élément recherché, on arrête.
+     * Si tab[m] se situe après l’élément recherché dans l’ordre lexicographique, on recommence
+     * la recherche sur le sous-tableau entre les indices 0 et m-1, sinon, on recommence sur le
+     * sous-tableau entre les indices m+1 et tab.length-1.
+     *
      * @param arrayNames
      * @param arrayNumbers
      * @param name
@@ -150,21 +151,21 @@ public class PhoneDirectory {
         int sup = 0;
 
         // First, we need to find the upper limit of the stored elements
-        for(int i = 0; i < arrayNames.length; i++){
-            if(arrayNames[i].equals("_END")){
+        for (int i = 0; i < arrayNames.length; i++) {
+            if (arrayNames[i].equals("_END")) {
                 sup = i - 1;
                 break;
             }
         }
 
-        while(inf <= sup){
+        while (inf <= sup) {
             int mid = (inf + sup) / 2;
             int comparisonResult = arrayNames[mid].toLowerCase().compareTo(name);
 
-            if(comparisonResult == 0){
+            if (comparisonResult == 0) {
                 // We found the name
                 return arrayNumbers[mid];
-            } else if(comparisonResult < 0){
+            } else if (comparisonResult < 0) {
                 // The name is after mid
                 inf = mid + 1;
             } else {
@@ -184,37 +185,37 @@ public class PhoneDirectory {
      */
     private static void addNewPersonPhoneDirectory(String[] arrayNames, String[] arrayNumbers, String nameToAdd, String numberToAdd) {
         String nameAlreadyAdded = searchPhoneDirectory(arrayNames, arrayNumbers, nameToAdd.toLowerCase());
-        if(!nameAlreadyAdded.isEmpty()){
+        if (!nameAlreadyAdded.isEmpty()) {
             // This means that the name exists already in the directory
             // We just have to update the number
             // We will loop in the arrays and find the name and update the number
-            for(int i = 0; i < arrayNames.length && !arrayNames[i].equals("_END"); i++){
-                if(arrayNames[i].equals(nameToAdd)){
+            for (int i = 0; i < arrayNames.length && !arrayNames[i].equals("_END"); i++) {
+                if (arrayNames[i].equals(nameToAdd)) {
                     // We found the name, so we update the number
                     arrayNumbers[i] = numberToAdd;
                     // We will break the loop since we have found the element to update
                     break;
                 }
             }
-        } else{
+        } else {
             // The name does not exist in the directory so we have to add it.
             int lastStoredIndex = 0;
 
             // We are going to loop in the arrays to find the element "_END"
-            for(int i = 0; i < arrayNames.length; i++){
-                if(arrayNames[i].equals("_END")){
+            for (int i = 0; i < arrayNames.length; i++) {
+                if (arrayNames[i].equals("_END")) {
                     lastStoredIndex = i;
                     break;
                 }
             }
 
             // Once we have found it, we check if we still have place to store the new person's details
-            if(lastStoredIndex < arrayNames.length - 1){
+            if (lastStoredIndex < arrayNames.length - 1) {
                 // We can add the name and the number
                 arrayNames[lastStoredIndex] = nameToAdd;
                 arrayNumbers[lastStoredIndex] = numberToAdd;
-                arrayNames[lastStoredIndex + 1 ] = "_END";
-                arrayNumbers[lastStoredIndex + 1 ] = "_END";
+                arrayNames[lastStoredIndex + 1] = "_END";
+                arrayNumbers[lastStoredIndex + 1] = "_END";
 
                 System.out.println(nameToAdd + " / " + numberToAdd + " was added successfully");
 
@@ -225,29 +226,31 @@ public class PhoneDirectory {
         }
     }
 
-    /** Bonus 1 : Delete a name from the directory
+    /**
+     * Bonus 1 : Delete a name from the directory
      * Écrire une action permettant de supprimer un enregistrement : demandez le nom,
      * vérifiez que le nom est bien dans le tableau puis décalez d’un cran à gauche toutes les
      * informations restantes. N’oubliez pas de modifier le menu proposé à l’utilisateur.
+     *
      * @param arrayNames
      * @param arrayNumbers
      * @param nameToSuppress
      */
-        private static void deletePersonPhoneDirectory(String[] arrayNames, String[] arrayNumbers, String nameToSuppress) {
+    private static void deletePersonPhoneDirectory(String[] arrayNames, String[] arrayNumbers, String nameToSuppress) {
         String nameAlreadyInTheDirectory = searchPhoneDirectory(arrayNames, arrayNumbers, nameToSuppress.toLowerCase());
 
-        if(!nameAlreadyInTheDirectory.isEmpty()){
+        if (!nameAlreadyInTheDirectory.isEmpty()) {
             // The name exists in the directory, so we have to delete it
-            for(int i = 0; i < arrayNames.length && !arrayNames[i].equals("_END"); i++){
-                if(arrayNames[i].equals(nameToSuppress)){
+            for (int i = 0; i < arrayNames.length && !arrayNames[i].equals("_END"); i++) {
+                if (arrayNames[i].equals(nameToSuppress)) {
                     // We found the name to delete
                     // We will shift all the next elements to the left by one position
-                    for(int j = i; j < arrayNames.length - 1; j++){
+                    for (int j = i; j < arrayNames.length - 1; j++) {
                         arrayNames[j] = arrayNames[j + 1];
                         arrayNumbers[j] = arrayNumbers[j + 1];
 
                         // If we reach _END, we can stop shifting
-                        if(arrayNames[j].equals("_END")){
+                        if (arrayNames[j].equals("_END")) {
                             break;
                         }
                     }
@@ -262,25 +265,27 @@ public class PhoneDirectory {
         }
     }
 
-    /** Bonus 2 : Sort the phone directory alphabetically */
+    /**
+     * Bonus 2 : Sort the phone directory alphabetically
+     */
     private static void sortPhoneDirectory(String[] arrayNames, String[] arrayNumbers) {
         // We will use a simple bubble sort algorithm
         boolean swapped;
         int length = 0;
 
         // First, we need to find the length of the stored elements
-        for(int i = 0; i < arrayNames.length; i++){
-            if(arrayNames[i].equals("_END")){
+        for (int i = 0; i < arrayNames.length; i++) {
+            if (arrayNames[i].equals("_END")) {
                 length = i;
                 break;
             }
         }
 
         // Now we can sort the arrays
-        do{
+        do {
             swapped = false;
-            for(int i = 0; i < length - 1; i++){
-                if(arrayNames[i].compareToIgnoreCase(arrayNames[i + 1]) > 0){
+            for (int i = 0; i < length - 1; i++) {
+                if (arrayNames[i].compareToIgnoreCase(arrayNames[i + 1]) > 0) {
                     // We need to swap the elements in both arrays
                     String tempName = arrayNames[i];
                     String tempNumber = arrayNumbers[i];
@@ -294,7 +299,7 @@ public class PhoneDirectory {
                     swapped = true;
                 }
             }
-        } while(swapped);
+        } while (swapped);
     }
 
     /**
@@ -304,6 +309,7 @@ public class PhoneDirectory {
      * ligne du fichier doit donc être :
      * DUPONT;06 70..44
      * Ajouter un choix dans le menu proposé à l’utilisateur.
+     *
      * @param arrayNames
      * @param arrayNumbers
      */
@@ -312,8 +318,8 @@ public class PhoneDirectory {
         StringBuilder sb = new StringBuilder();
 
         // We loop through the arrays to build the content
-        for(int i = 0; i < arrayNames.length; i++){
-            if(!arrayNames[i].equals("_END")){
+        for (int i = 0; i < arrayNames.length; i++) {
+            if (!arrayNames[i].equals("_END")) {
                 sb.append(arrayNames[i]).append(";").append(arrayNumbers[i]).append("\n");
             } else {
                 break;
@@ -335,6 +341,7 @@ public class PhoneDirectory {
     /**
      * Écrire une action importer qui fait l'opération inverse en récupérant ce qui est dans
      * le fichier pour le copier dans le tableau. N'oubliez pas d'ajouter les chaînes "_FIN".
+     *
      * @param arrayNames
      * @param arrayNumbers
      */
@@ -347,10 +354,10 @@ public class PhoneDirectory {
             int index = 0;
 
             // We read the file line by line
-            while(fileScanner.hasNextLine() && index < arrayNames.length - 1){
+            while (fileScanner.hasNextLine() && index < arrayNames.length - 1) {
                 String line = fileScanner.nextLine();
                 String[] parts = line.split(";");
-                if(parts.length == 2){
+                if (parts.length == 2) {
                     arrayNames[index] = parts[0];
                     arrayNumbers[index] = parts[1];
                     index++;
